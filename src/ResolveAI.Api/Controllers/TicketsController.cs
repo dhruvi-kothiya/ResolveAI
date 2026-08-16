78e54274dddf508e51b5b49cf7447dfc4920e048
Dhruvi Kothiya <dhruvikothiya0@gmail.com>
2026-08-16 18:29:08 +0530

    feat: ticket creation system working (INC-1001)

---
+++ src/ResolveAI.Api/Controllers/TicketsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResolveAI.Application.DTOs;
+using ResolveAI.Domain.Entities;
+using ResolveAI.Domain.Enums;
+using ResolveAI.Infrastructure.Persistence;
+
+namespace ResolveAI.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TicketsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public TicketsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTicket([FromBody] CreateTicketRequest request)
+    {
+        // ૧. ticket nu generet (INC-XXXX)
+        var ticketCount = _context.Tickets.Count() + 1;
+        var ticketNumber = $"INC-{1000 + ticketCount}";
+
+        // ૨.creat new ticket obj
+        var ticket = new Ticket
+        {
+            Id = Guid.NewGuid(),
+            TicketNumber = ticketNumber,
+            Title = request.Title,
+            Description = request.Description,
+            Status = TicketStatus.New, // new ticket new
+            Priority = TicketPriority.Medium, // Default 
+            CreatedById = request.CreatedById,
+            DepartmentId = request.DepartmentId,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        _context.Tickets.Add(ticket);
+        await _context.SaveChangesAsync();
+
+        return Ok(new
+        {
+            Message = "Ticket Created Successfully!",
+            TicketNumber = ticket.TicketNumber
+        });
+    }
+}
---
+++ src/ResolveAI.Application/DTOs/CreateTicketRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ResolveAI.Application.DTOs;
+
+public class CreateTicketRequest
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Guid DepartmentId { get; set; } // department problem
+    public Guid CreatedById { get; set; } // who ticket creat
+}
